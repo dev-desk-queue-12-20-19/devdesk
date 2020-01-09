31507f7639b698f46b0d90f8ea66724c0be8b970
Chinedu Orie <nedsoftdeveloper@gmail.com>
2020-01-09 19:29:17 +0100

    Fix deploy error

--- data/dbConfig.js
+++ data/dbConfig.js
@@ -1,8 +1,14 @@
+// const knex = require('knex');
+// require('dotenv').config();
+
+// const dbEngine = process.env.NODE_ENV.toLowerCase() || 'development';
+// const knexConfig = require('../knexfile')[dbEngine];
+
+// module.exports = knex(knexConfig);
+
+
 const knex = require('knex');
-require('dotenv').config();
 
-const dbEngine = process.env.NODE_ENV || 'development';
-const knexConfig = require('../knexfile')[dbEngine];
+const knexConfig = require('../knexfile.js');
 
-
-module.exports = knex(knexConfig);
+module.exports = knex(knexConfig.development);
